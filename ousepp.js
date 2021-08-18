57759f926b038519f461d101065e63f228615f56
Yoosuf Mohammed Shaheed <77971881+ItsYoosuf@users.noreply.github.com>
2021-08-18 18:22:40 +0530

    Add files via upload

---
+++ ousepp.js
@@ -0,0 +1,17 @@
+function makeOne(){
+    document.body.style.backgroundColor="#e27d60";
+}
+
+function makeTwo(){
+    document.body.style.backgroundColor="#85dcb8";  
+}
+
+function makeThree(){
+    document.body.style.backgroundColor="#41B3A3";  
+}
+
+function makeFour(){
+    document.body.style.backgroundColor="#E8A87C";  
+}
+
+
